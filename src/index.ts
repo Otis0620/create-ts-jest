e77069ee4be7bc63dcfe32c50bf21528bdd9b5b8
Otis0620 <sharding1012@gmail.com>
2019-12-19 23:57:22 -0800

    create prettier file generator and template

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-export function helloWorld(str: String) {
-  return `hello ${str}`;
+const fs = require('fs');
+
+function createDir(directoryPath: string) {
+  try {
+    fs.mkdirSync(directoryPath, { recursive: true });
+  } catch (err) {
+    console.error('An error has occurred: ', err);
+  }
 }
+
+function createPrettierConfig(dir: string) {
+  const templatesPath = `${process.cwd()}/src/templates/prettier.txt`;
+  try {
+    const prettierConfig = fs.readFileSync(templatesPath, 'utf8');
+
+    fs.writeFileSync(`${dir}/.prettierrc`, prettierConfig);
+  } catch (error) {
+    console.error('an error has occured ', error);
+  }
+}
+
+(function main(directoryPath: string) {
+  const newDirectory = `${process.cwd()}/${directoryPath}`;
+
+  createDir(newDirectory);
+  createPrettierConfig(newDirectory);
+})('test');
