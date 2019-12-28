c91fca0eaf40038813eb778f46fa257b70235b08
Otis0620 <sharding1012@gmail.com>
2019-12-27 22:25:22 -0800

    add logic for retrieving command-line argument

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,18 @@ import { createAllConfigs, createDirectory, createSrc } from '@helpers/index';
 /**
  * Main method for running all the code.
  */
-(function main(directoryPath: string) {
-  const newDirectory = `${process.cwd()}/${directoryPath}`;
+(function main() {
+  const projectName = process.argv[2];
+
+  if (!projectName) {
+    console.error('Please provide a name for the project');
+    return;
+  }
+
+  const newDirectory = `${process.cwd()}/${projectName}`;
   const templates = `${process.cwd()}/src/templates`;
 
   createDirectory(newDirectory);
   createSrc(newDirectory);
   createAllConfigs(newDirectory, templates);
-})('test');
+})();
